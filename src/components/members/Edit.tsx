8c5e05592a3db113a4555388cb644ba7ad518baf
farildzaky <alfarildzaky@gmail.com>
2025-09-11 23:39:53 +0700

    refactor: admin members to lawyers

--- src/components/members/Edit.tsx
+++ src/components/members/Edit.tsx
@@ -504,7 +504,7 @@ const EditMemberPage = () => {
 
                         <DynamicListSection title="Work Experience" list={practiceFocus} setList={setPracticeFocus} />
                         <DynamicListSection title="Education" list={education} setList={setEducation} />
-                        <DynamicListSection title="Language" list={language} setList={setLanguage} />
+                        <DynamicListSection title="Remarks" list={language} setList={setLanguage} />
                     </div>
 
                     <div className="flex justify-end mt-8">
